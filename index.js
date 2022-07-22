c4124d3a39cb6e3229312491f6b925f4d1da8df8
Rajiv Ranjan <rajiv.ranjan@nitorinfotech.net>
2022-07-22 11:38:53 +0530

    validation of login form

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-//import userDetail from './user.json'
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+if(getCookie("username") != ""){
+    location.assign("signout.html")
+}
+
+
 
 const btn = document.getElementById("loginbtn");
 btn.addEventListener('click',(e)=>{
     const username = document.getElementById('username').value;
     const pswrd = document.getElementById('pswrd').value;
     if(username=="admin" && pswrd == "12345" || username=="add" && pswrd == "123"){
-        //alert("login success");
-        //return false;
+        document.cookie = "username = admin "; 
         location.assign("signout.html");
-        
-    }else{
+    }
+    else{
         alert("failed");
+        location.assign("index.html")
     }
 })
+
 
 
 
